3a6cd65ca4c9c5f0baa53eb974de1c918f9afded
Shiraz Khan <shyrazkhan@hotmail.com>
2022-11-27 04:04:34 +0500

    Asp.Net Core CRUD application with Docker baseline

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using DockerCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerCrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context;
+        public HomeController(MovieContext mc)
+        {
+            context = mc;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Movie m)
+        {
+            if (ModelState.IsValid)
+            {               
+
+                context.Add(m);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult Index()
+        {
+            var m = context.Movie.ToList();
+            return View(m);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var pc = context.Movie.Where(a => a.Id == id).FirstOrDefault();
+            return View(pc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(m);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View(m);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var pc = context.Movie.Where(a => a.Id == id).FirstOrDefault();
+            context.Remove(pc);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerCrud.Models
+{
+    public class Movie
+    {        
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Actors { get; set; }
+    }
+}
