803fdf6dac740530cfb24159282ab0d03d99dbb5
Shiraz Khan <shyrazkhan@hotmail.com>
2022-11-27 12:36:22 +0500

    Updated dbScript.

--- dbScript.sql
+++ dbScript.sql
@@ -1,6 +1,25 @@
-CREATE TABLE [dbo].[Movie](
+USE [master]
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE [name] = N'MovieDB') 
+BEGIN
+	PRINT N'Creating MovieDB Database...';
+	CREATE DATABASE [MovieDB]
+END
+GO
+
+USE [MovieDB]
+GO
+
+
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE [TABLE_NAME] = N'Movie' AND [TABLE_TYPE] = N'BASE TABLE') 
+BEGIN
+	PRINT N'Creating [Movie]...';
+	CREATE TABLE [dbo].[Movie](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](400) NOT NULL,
 	[Actors] [nvarchar](400) NOT NULL
 ) ON [PRIMARY]
+	
+END
 GO
